5ab847e0b5043e25716efb706b13222445b366bb
Vodka <70968395+cat4walking@users.noreply.github.com>
2021-05-14 23:00:51 +0700

    add bootrap 4

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+const handlebars = require('express-handlebars');
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined'));
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
---
+++ src/resource/views/partials/footer.hbs
@@ -0,0 +1,3 @@
+<h2>
+    This is footer
+</h2>
