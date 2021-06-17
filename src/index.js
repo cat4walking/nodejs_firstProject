491148a3191f07b26e5dc215d3bb6aba08f09047
Vodka <70968395+cat4walking@users.noreply.github.com>
2021-06-17 23:53:18 +0700

    use body parser

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ const path = require('path');
 const morgan = require('morgan');
 const app = express();
 const handlebars = require('express-handlebars');
+const { Console } = require('console');
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('combined'));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 app.engine('hbs', handlebars({
     extname: '.hbs'
 }));
@@ -21,6 +26,9 @@ app.get('/news', (req, res) => {
 });
 app.get('/search', (req, res) => {
     res.render('search');
+});
+app.post('/search', (req, res) => {
+    res.send('');
 });
 
 app.listen(port, () => {
